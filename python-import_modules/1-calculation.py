e70d51bde49011864ce09743a9aff84d308c5c19
laledsja2188 <ceferovalale343@gmail.com>
2025-11-03 23:49:30 +0400

    Fix pycodestyle errors in 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    
+
     a = 10
     b = 5
-    
+
     addition = add(a, b)
     subtraction = sub(a, b)
     multiplication = mul(a, b)
     division = div(a, b)
-    
+
     print("{} + {} = {}".format(a, b, addition))
     print("{} - {} = {}".format(a, b, subtraction))
     print("{} * {} = {}".format(a, b, multiplication))
