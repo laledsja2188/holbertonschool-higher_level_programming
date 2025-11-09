cdee28cfffc3fe99481002d12615ede8f8fd6c32
laledsja2188 <ceferovalale343@gmail.com>
2025-11-09 21:31:20 +0400

    Add safe_print_list function

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def siyahi(my_list=[], x=0):
+    say = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            say += 1
+        except IndexError:
+            break
+    print()
+    return say
