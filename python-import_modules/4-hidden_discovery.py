9240a40540d5679f7d5e48c124026facec0a1f5e
laledsja2188 <ceferovalale343@gmail.com>
2026-01-23 09:42:49 +0400

    Update 4-hidden_discovery.py - fix formatting and improve sorting logic

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    
+
     all_names = dir(hidden_4)
-    
-    for name in all_names:
-        first_two_chars = name[:2]
-        if first_two_chars != "__":
+
+    for name in sorted(all_names):
+        if not name.startswith("__"):
             print(name)
